64e634ee8bc5fe0208ae4bc6e21641785f64376f
木子空间 <68138841+yan-sheng-li@users.noreply.github.com>
2025-06-20 02:40:48 +0800

    Update watermark.js

--- watermark.js
+++ watermark.js
@@ -1,9 +1,9 @@
 (function () {
     // 设置水印文本内容，可以设置为两行或更多
-    const watermarkText = ['项目定做、远程调试', 'www.liyansheng.top'];  // 水印内容数组，第一行和第二行文字
+    const watermarkText = ['项目定做、远程调试', 'by 木子空间'];  // 水印内容数组，第一行和第二行文字
 
     // 设置水印透明度（值越小透明度越高）
-    const watermarkOpacity = 0.35;
+    const watermarkOpacity = 0.40;
 
     // 设置右下角悬浮图片、链接的配置
     const floatingImage = {
